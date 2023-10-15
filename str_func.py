8f608aa7b7d9d94d3ada0711f7b4de5619cf0120
User <git@github.com:AlDrag91/tes.git>
2023-10-16 02:58:59 +0400

    2 функции

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
 def upper_func(line: str):
     upper_case = line.upper()
     return upper_case
+
+def title_func(line: str):
+    """Функция заглавныме первые буквы"""
+    title_case = line.title()
+    return title_case
