472f9695fb068fb7bab62ecbd1e62cb4428964dc
AlDrag91 <147440379+AlDrag91@users.noreply.github.com>
2023-10-25 11:09:31 +0400

    Update str_func.py
    Без docstring

--- str_func.py
+++ str_func.py
@@ -1,9 +1,9 @@
 def upper_func(line: str):
-    """Функция все большие буквы"""
+    
     upper_case = line.upper()
     return upper_case
 
 def title_func(line: str):
-    """Функция заглавныме первые буквы11111"""
+    
     title_case = line.title()
     return title_case
