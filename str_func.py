46a48c6da05a74ee52d0f62492b50b8e7e26890b
User <git@github.com:AlDrag91/tes.git>
2023-10-25 12:31:16 +0400

    2 Функции 1 docstring

--- str_func.py
+++ str_func.py
@@ -2,7 +2,6 @@ def upper_func(line: str):
     upper_case = line.upper()
     return upper_case
 
-
 def title_func(line: str):
     """Делает заглавными первые буквы каждого слова в строке."""
     title_case = line.title()
