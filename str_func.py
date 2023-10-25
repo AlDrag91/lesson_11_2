d9ac7af1e244bef6a327c9e4d5104fe8ff4ddb76
AlDrag91 <147440379+AlDrag91@users.noreply.github.com>
2023-10-25 17:40:51 +0400

    Update str_func.py
    Изменение стороннего пользователя

--- str_func.py
+++ str_func.py
@@ -1,8 +1,5 @@
 def upper_func(line: str):
+    """ docstring """
     upper_case = line.upper()
     return upper_case
 
-def title_func(line: str):
-    """Делает заглавными первые буквы каждого слова в строке."""
-    title_case = line.title()
-    return title_case
